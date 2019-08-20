b35d4b6592e61c6619a0a5c545d996abd1bb77f4
g1 <guyangbin@kenfeng.com>
2019-08-20 15:08:07 +0800

    增加setHeader的测试

--- feign-demo-client/src/main/java/com/example/feignclient/democlient/DemoClientApplication.java
+++ feign-demo-client/src/main/java/com/example/feignclient/democlient/DemoClientApplication.java
@@ -12,6 +12,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
 
 @SpringBootApplication
@@ -41,6 +43,16 @@ public class DemoClientApplication {
 		return person;
 	}
 
+
+	@RequestMapping("/person/getwithheader")
+	public String queryPersonSetHeader(HttpServletRequest request, HttpServletResponse response){
+		String person = personService.getPerson();
+		response.setHeader("referer","bbb");
+		response.setHeader("appId","bbb");
+		log.info("client query person is {}",person);
+		return person;
+
+	}
 //	@Component
 //	public class StoreIntegration {
 //		@HystrixCommand(fallbackMethod = "defaultStores")
